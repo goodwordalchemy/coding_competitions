7742b3fb563c83ee75571853c49fa2f1979ee93b
David Goldberg <goodwordachemy@gmail.com>
2020-05-24 13:18:03 -0700

    got a bunch of print statements but fixed 2 test case

--- kickstartH2019/elevanagram.py
+++ kickstartH2019/elevanagram.py
@@ -11,15 +11,18 @@ sample_text = """6
 0 0 0 0 0 0 0 1 0"""
 
 samples = [
-    ("0 0 2 0 0 1 0 0 0", "YES"),
-    ("0 0 0 0 0 0 0 0 12", "YES"),
-    ("0 0 0 0 2 0 1 1 0", "NO"),
-    ("3 1 1 1 0 0 0 0 0", "YES"),
-    ("3 0 0 0 0 0 3 0 2", "YES"),
-    ("0 0 0 0 0 0 0 1 0", "NO"),
-    ("0 0 0 0 0 0 0 0 0", "YES"),
-    ("2 0 0 0 0 0 0 0 0", "YES"),
-    ("3 0 0 0 0 0 0 0 0", "NO"),
+    # ("0 0 2 0 0 1 0 0 0", "YES"),
+    # ("0 0 0 0 0 0 0 0 12", "YES"),
+    # ("0 0 0 0 2 0 1 1 0", "NO"),
+    # ("3 1 1 1 0 0 0 0 0", "YES"),
+    # ("3 0 0 0 0 0 3 0 2", "YES"),
+    # ("0 0 0 0 0 0 0 1 0", "NO"),
+    # ("0 0 0 0 0 0 0 0 0", "YES"),
+    # ("2 0 0 0 0 0 0 0 0", "YES"),
+    # ("3 0 0 0 0 0 0 0 0", "NO"),
+    # ("0 0 0 0 0 0 0 8 9", "NO"),
+    # ("0 1 0 0 0 0 0 0 0", "NO"),
+    ("0 0 0 1 0 0 0 0 1", "NO"),
 ]
 sample_inputs, sample_outputs = zip(*samples)
 
@@ -49,17 +52,33 @@ def parse_input():
     return test_cases
 
 def can_elevenagram(A):
-    @lru_cache(None)
+    mem = {}
+    from pprint import pprint
+    #@lru_cache(None)
     def dp(i, j, k):
+        if (i, j, k) in mem:
+            return mem[i,j,k]
+        if j < 0:
+            return False
+
         if i == 0:
-            return (2*j - A[0]) % 11 == k
+            mem[i,j,k] = (k - (2*j - A[0])) % 11 == 0 
+            # print("dp called with...",i, j, k)
+            # pprint(mem)
+            return mem[i,j,k]
 
         for p in range(A[i]+1):
             sum_ = k - (i+1) * (2*p-A[i])
             if dp(i-1, j-p, sum_ % 11):
-                return True
+                mem[i,j,k] =  True
+                # print("dp called with...",i, j, k)
+                # pprint(mem)
+                return mem[i,j,k]
 
-        return False
+        mem[i,j,k] = False
+        # print("dp called with...",i, j, k)
+        # pprint(mem)
+        return mem[i,j,k]
 
     result = dp(8, sum(A)//2, 0)
 
@@ -69,5 +88,28 @@ def main():
     for i, test_case in enumerate(parse_input()):
         print("Case #{}: {}".format(i+1, can_elevenagram(test_case)))
 
+def find_fuckups(low, high):
+    from collections import Counter
+
+    def is_valid_sample(n:str):
+        return "0" not in n
+
+    def get_sample(n:str):
+        c = Counter(n)
+        case = [c[str(i)] for i in range(1, 10)]
+        ans = "YES" if int(n) % 11 == 0 else "NO"
+        return case, ans
+
+    for i in range(low, high):
+        n = str(i)
+        if not is_valid_sample(n):
+            continue
+        case, ans = get_sample(n)
+        result = can_elevenagram(case)
+        # print(case)
+        print("i:{}, expect: {}, wegood?: {}".format(i, ans, result==ans))
+
+
 if __name__ == '__main__':
     main()
+    # find_fuckups(0, 100)
